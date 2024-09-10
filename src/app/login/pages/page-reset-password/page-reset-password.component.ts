db78af6aa9f2a48493d5f5ffd249c2a7030bffd5
Christian Lisangola <jochri3@gmail.com>
2024-09-10 10:31:48 +0300

    3.Compodoc

---
+++ src/app/login/pages/page-reset-password/page-reset-password.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-reset-password',
+  templateUrl: './page-reset-password.component.html',
+  styleUrl: './page-reset-password.component.scss'
+})
+export class PageResetPasswordComponent {
+
+}
