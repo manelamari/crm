d619ef6f9dd8ed2734e4c4c64d5f8bd117c4113b
Christian Lisangola <jochri3@gmail.com>
2024-09-10 17:30:35 +0300

    ngclass/event binding

--- src/app/ui/components/ui/ui.component.ts
+++ src/app/ui/components/ui/ui.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ui',
   templateUrl: './ui.component.html',
-  styleUrl: './ui.component.scss'
+  styleUrl: './ui.component.scss',
 })
 export class UiComponent {
-
+  public open: boolean = true;
+  public toggle(): void {
+    this.open = !this.open;
+    // console.log(`Open : ${this.open}`);
+  }
 }
