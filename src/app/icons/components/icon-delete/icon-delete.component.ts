d12f200cb826482308471a869baff9439d40f35a
Christian Lisangola <jochri3@gmail.com>
2024-09-11 10:32:12 +0300

    10.2.icons components et FontAwesome

--- src/app/icons/components/icon-delete/icon-delete.component.ts
+++ src/app/icons/components/icon-delete/icon-delete.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-icon-delete',
   templateUrl: './icon-delete.component.html',
-  styleUrl: './icon-delete.component.scss'
+  styleUrl: './icon-delete.component.scss',
 })
 export class IconDeleteComponent {
-
+  public myIcon = faTrash;
 }
